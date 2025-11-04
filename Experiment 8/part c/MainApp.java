8d653ea8034af081d7793ee7c442307e92303f9d
Shubham18Raj <shubhamsingh45689@gmail.com>
2025-11-04 22:14:16 +0530

    Update MainApp.java

--- Experiment 8/part c/MainApp.java
+++ Experiment 8/part c/MainApp.java
@@ -1,1 +1,14 @@
+package com.example.springhibernate;
 
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class MainApp {
+    public static void main(String[] args) {
+        AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(AppConfig.class);
+        AccountService service = context.getBean(AccountService.class);
+
+        service.transfer(101, 102, 500.0); // Transfer ₹500 from 101 → 102
+
+        context.close();
+    }
+}
