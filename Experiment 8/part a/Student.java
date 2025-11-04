ef9c2e321ca1e459b24c366cc536aa3ce8aa59d8
Shubham18Raj <shubhamsingh45689@gmail.com>
2025-11-04 20:18:00 +0530

    Update Student.java

--- Experiment 8/part a/Student.java
+++ Experiment 8/part a/Student.java
@@ -1,1 +1,14 @@
+package com.example.springdi;
+
+public class Student {
+    private Course course;
+
+    public Student(Course course) { // Constructor-based Injection
+        this.course = course;
+    }
+
+    public void showInfo() {
+        System.out.println("Student enrolled in course: " + course.getCourseName());
+    }
+}
 
