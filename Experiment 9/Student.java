a8b6d6180f9d3f2c18389461b6bf34e41d0a56c4
Shubham18Raj <shubhamsingh45689@gmail.com>
2025-11-04 22:23:13 +0530

    Update Student.java

--- Experiment 9/Student.java
+++ Experiment 9/Student.java
@@ -1,1 +1,19 @@
+@Entity
+@Table(name = "students")
+public class Student {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int studentId;
+
+    @Column(nullable = false)
+    private String name;
+
+    @ManyToOne
+    @JoinColumn(name = "course_id")
+    private Course course;
+
+    private double balance;
+
+    // Getters, Setters, Constructors
+}
 
