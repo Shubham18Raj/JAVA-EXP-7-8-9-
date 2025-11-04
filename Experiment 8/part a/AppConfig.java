93b3eb5fefed890707938bd81ff00a5326832039
Shubham18Raj <shubhamsingh45689@gmail.com>
2025-11-04 20:19:04 +0530

    Update AppConfig.java

--- Experiment 8/part a/AppConfig.java
+++ Experiment 8/part a/AppConfig.java
@@ -1,1 +1,17 @@
+package com.example.springdi;
 
+import org.springframework.context.annotation.*;
+
+@Configuration
+public class AppConfig {
+
+    @Bean
+    public Course course() {
+        return new Course("Spring Framework");
+    }
+
+    @Bean
+    public Student student() {
+        return new Student(course());
+    }
+}
