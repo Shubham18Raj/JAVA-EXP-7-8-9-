cd16accf6713df052e0a076391354e98bbe1eef9
Shubham18Raj <shubhamsingh45689@gmail.com>
2025-11-04 20:21:03 +0530

    Update Student.java

--- Experiment 8/part b/Student.java
+++ Experiment 8/part b/Student.java
@@ -1,1 +1,26 @@
+package com.example.hibernatecrud;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "Student")
+public class Student {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @Column
+    private String name;
+
+    @Column
+    private String city;
+
+    // Getters & Setters
+    public int getId() { return id; }
+    public void setName(String name) { this.name = name; }
+    public String getName() { return name; }
+    public void setCity(String city) { this.city = city; }
+    public String getCity() { return city; }
+}
 
