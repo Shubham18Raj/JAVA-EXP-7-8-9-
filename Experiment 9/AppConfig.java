5575b55e80f617a2aca9fa2e9f1835ba0cb1615f
Shubham18Raj <shubhamsingh45689@gmail.com>
2025-11-04 22:20:32 +0530

    Update AppConfig.java

--- Experiment 9/AppConfig.java
+++ Experiment 9/AppConfig.java
@@ -1,1 +1,20 @@
+@Configuration
+@ComponentScan(basePackages = "com.studentmanagement")
+public class AppConfig {
+
+    @Bean
+    public SessionFactory sessionFactory() {
+        return new Configuration().configure("hibernate.cfg.xml").buildSessionFactory();
+    }
+
+    @Bean
+    public StudentDAO studentDAO() {
+        return new StudentDAO();
+    }
+
+    @Bean
+    public FeeService feeService() {
+        return new FeeService();
+    }
+}
 
