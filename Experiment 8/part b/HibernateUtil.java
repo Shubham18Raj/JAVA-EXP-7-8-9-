6c8615fe428c36b8b546e094f1f598c307051fa6
Shubham18Raj <shubhamsingh45689@gmail.com>
2025-11-04 20:22:55 +0530

    Update HibernateUtil.java

--- Experiment 8/part b/HibernateUtil.java
+++ Experiment 8/part b/HibernateUtil.java
@@ -1,1 +1,17 @@
+package com.example.hibernatecrud;
+
+import org.hibernate.*;
+import org.hibernate.cfg.Configuration;
+
+public class HibernateUtil {
+    private static SessionFactory factory;
+
+    static {
+        factory = new Configuration().configure("hibernate.cfg.xml").buildSessionFactory();
+    }
+
+    public static SessionFactory getSessionFactory() {
+        return factory;
+    }
+}
 
