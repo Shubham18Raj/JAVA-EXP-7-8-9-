7ff764e005349b370f2926bc65ab86738932326b
Shubham18Raj <shubhamsingh45689@gmail.com>
2025-11-04 22:26:13 +0530

    Update StudentDAO.java

--- Experiment 9/StudentDAO.java
+++ Experiment 9/StudentDAO.java
@@ -1,1 +1,38 @@
+@Repository
+public class StudentDAO {
+    @Autowired
+    private SessionFactory sessionFactory;
+
+    public void saveStudent(Student student) {
+        Session session = sessionFactory.openSession();
+        Transaction tx = session.beginTransaction();
+        session.save(student);
+        tx.commit();
+        session.close();
+    }
+
+    public Student getStudent(int id) {
+        Session session = sessionFactory.openSession();
+        Student student = session.get(Student.class, id);
+        session.close();
+        return student;
+    }
+
+    public void updateStudent(Student student) {
+        Session session = sessionFactory.openSession();
+        Transaction tx = session.beginTransaction();
+        session.update(student);
+        tx.commit();
+        session.close();
+    }
+
+    public void deleteStudent(int id) {
+        Session session = sessionFactory.openSession();
+        Transaction tx = session.beginTransaction();
+        Student student = session.get(Student.class, id);
+        if (student != null) session.delete(student);
+        tx.commit();
+        session.close();
+    }
+}
 
