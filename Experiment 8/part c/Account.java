e0fd40b874e5a75f06fb686f08d3af9bcf049a5e
Shubham18Raj <shubhamsingh45689@gmail.com>
2025-11-04 22:10:31 +0530

    Update Account.java

--- Experiment 8/part c/Account.java
+++ Experiment 8/part c/Account.java
@@ -1,1 +1,22 @@
+package com.example.springhibernate;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "Account")
+public class Account {
+    @Id
+    private int accNo;
+
+    private String holderName;
+    private double balance;
+
+    // Getters and Setters
+    public int getAccNo() { return accNo; }
+    public void setAccNo(int accNo) { this.accNo = accNo; }
+    public String getHolderName() { return holderName; }
+    public void setHolderName(String holderName) { this.holderName = holderName; }
+    public double getBalance() { return balance; }
+    public void setBalance(double balance) { this.balance = balance; }
+}
 
