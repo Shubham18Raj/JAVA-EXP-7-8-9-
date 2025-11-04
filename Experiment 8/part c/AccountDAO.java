f05ac610a1b040a1d19e0ef3a560b08250da2337
Shubham18Raj <shubhamsingh45689@gmail.com>
2025-11-04 22:11:35 +0530

    Update AccountDAO.java

--- Experiment 8/part c/AccountDAO.java
+++ Experiment 8/part c/AccountDAO.java
@@ -1,1 +1,28 @@
+package com.example.springhibernate;
+
+import jakarta.transaction.Transactional;
+import org.hibernate.*;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class AccountDAO {
+
+    @Autowired
+    private SessionFactory factory;
+
+    @Transactional
+    public void updateBalance(int accNo, double amount) {
+        Session session = factory.getCurrentSession();
+        Account acc = session.get(Account.class, accNo);
+        acc.setBalance(acc.getBalance() + amount);
+        session.update(acc);
+    }
+
+    public double getBalance(int accNo) {
+        Session session = factory.getCurrentSession();
+        Account acc = session.get(Account.class, accNo);
+        return acc.getBalance();
+    }
+}
 
