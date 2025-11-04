966d545bbf9b536b4bf585513079a5bc2113887b
Shubham18Raj <shubhamsingh45689@gmail.com>
2025-11-04 22:12:30 +0530

    Update AccountService.java

--- Experiment 8/part c/AccountService.java
+++ Experiment 8/part c/AccountService.java
@@ -1,1 +1,20 @@
+package com.example.springhibernate;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+public class AccountService {
+
+    @Autowired
+    private AccountDAO dao;
+
+    @Transactional
+    public void transfer(int fromAcc, int toAcc, double amount) {
+        dao.updateBalance(fromAcc, -amount);
+        dao.updateBalance(toAcc, amount);
+        System.out.println("Transferred " + amount + " successfully.");
+    }
+}
 
