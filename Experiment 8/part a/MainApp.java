b8a1ee3c61f6d32ada345ad35f4e4fef3c9d6b7f
Shubham18Raj <shubhamsingh45689@gmail.com>
2025-11-04 20:19:45 +0530

    Update MainApp.java

--- Experiment 8/part a/MainApp.java
+++ Experiment 8/part a/MainApp.java
@@ -1,1 +1,13 @@
+package com.example.springdi;
+
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class MainApp {
+    public static void main(String[] args) {
+        AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(AppConfig.class);
+        Student student = context.getBean(Student.class);
+        student.showInfo();
+        context.close();
+    }
+}
 
