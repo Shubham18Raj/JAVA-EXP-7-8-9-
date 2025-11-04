16e3d726f940c5421c6c3f26eb9c0943ecef639b
Shubham18Raj <shubhamsingh45689@gmail.com>
2025-11-04 22:28:57 +0530

    Update MainApp.java

--- Experiment 9/MainApp.java
+++ Experiment 9/MainApp.java
@@ -1,1 +1,26 @@
+public class MainApp {
+    public static void main(String[] args) {
+        AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(AppConfig.class);
+
+        StudentDAO studentDAO = context.getBean(StudentDAO.class);
+        FeeService feeService = context.getBean(FeeService.class);
+
+        // Add new student
+        Course course = new Course();
+        course.setCourseName("Java Development");
+        course.setDuration("6 Months");
+
+        Student student = new Student();
+        student.setName("Alice");
+        student.setCourse(course);
+        student.setBalance(10000);
+
+        studentDAO.saveStudent(student);
+
+        // Process payment
+        feeService.processPayment(student.getStudentId(), 2000);
+
+        context.close();
+    }
+}
 
