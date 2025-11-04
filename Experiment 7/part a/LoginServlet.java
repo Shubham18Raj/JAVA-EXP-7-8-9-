f0639c7a4f3cd5e2fad5783178b9d57af3114cdb
Shubham18Raj <shubhamsingh45689@gmail.com>
2025-11-04 19:58:57 +0530

    Update LoginServlet.java

--- Experiment 7/part a/LoginServlet.java
+++ Experiment 7/part a/LoginServlet.java
@@ -1,1 +1,23 @@
+import java.io.*;
+import javax.servlet.*;
+import javax.servlet.http.*;
+
+public class LoginServlet extends HttpServlet {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+
+        response.setContentType("text/html");
+        PrintWriter out = response.getWriter();
+
+        String user = request.getParameter("username");
+        String pass = request.getParameter("password");
+
+        // Hardcoded validation
+        if (user.equals("admin") && pass.equals("1234")) {
+            out.println("<h2>Welcome, " + user + "!</h2>");
+        } else {
+            out.println("<h2>Invalid username or password!</h2>");
+        }
+    }
+}
 
