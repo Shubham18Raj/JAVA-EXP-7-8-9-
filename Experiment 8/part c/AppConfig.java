023b9c00076fde12e690ffe8ea543c194c1cc921
Shubham18Raj <shubhamsingh45689@gmail.com>
2025-11-04 22:13:23 +0530

    Update AppConfig.java

--- Experiment 8/part c/AppConfig.java
+++ Experiment 8/part c/AppConfig.java
@@ -1,1 +1,38 @@
+package com.example.springhibernate;
+
+import org.hibernate.SessionFactory;
+import org.springframework.context.annotation.*;
+import org.springframework.orm.hibernate5.LocalSessionFactoryBean;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+import javax.sql.DataSource;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import java.util.Properties;
+
+@Configuration
+@ComponentScan("com.example.springhibernate")
+@EnableTransactionManagement
+public class AppConfig {
+
+    @Bean
+    public DataSource dataSource() {
+        DriverManagerDataSource ds = new DriverManagerDataSource();
+        ds.setDriverClassName("com.mysql.cj.jdbc.Driver");
+        ds.setUrl("jdbc:mysql://localhost:3306/bankdb");
+        ds.setUsername("root");
+        ds.setPassword("password");
+        return ds;
+    }
+
+    @Bean
+    public LocalSessionFactoryBean sessionFactory() {
+        LocalSessionFactoryBean sf = new LocalSessionFactoryBean();
+        sf.setDataSource(dataSource());
+        sf.setPackagesToScan("com.example.springhibernate");
+        Properties props = new Properties();
+        props.put("hibernate.dialect", "org.hibernate.dialect.MySQLDialect");
+        props.put("hibernate.hbm2ddl.auto", "update");
+        sf.setHibernateProperties(props);
+        return sf;
+    }
+}
 
