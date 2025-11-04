bca2d2fe9adfa578856a9ab0b45aa012f8d05648
Shubham18Raj <shubhamsingh45689@gmail.com>
2025-11-04 22:25:20 +0530

    Update Course.java

--- Experiment 9/Course.java
+++ Experiment 9/Course.java
@@ -1,1 +1,15 @@
+@Entity
+@Table(name = "courses")
+public class Course {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int courseId;
+
+    @Column(nullable = false)
+    private String courseName;
+
+    private String duration;
+
+    // Getters, Setters, Constructors
+}
 
