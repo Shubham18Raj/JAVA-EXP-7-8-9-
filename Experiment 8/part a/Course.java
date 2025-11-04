65fe143aa60138541337e38d66cfc8217295da9e
Shubham18Raj <shubhamsingh45689@gmail.com>
2025-11-04 20:17:16 +0530

    Update Course.java

--- Experiment 8/part a/Course.java
+++ Experiment 8/part a/Course.java
@@ -1,1 +1,14 @@
+package com.example.springdi;
+
+public class Course {
+    private String courseName;
+
+    public Course(String courseName) {
+        this.courseName = courseName;
+    }
+
+    public String getCourseName() {
+        return courseName;
+    }
+}
 
